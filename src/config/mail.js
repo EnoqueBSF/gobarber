e30d30e58d13901644c5e552f8c730b06a90c9df
Enoque Belmiro dos Santos Filho <enoquebelmiro@hotmail.com>
2020-02-19 00:10:01 -0300

    Implementation execution queue with redis and update application

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '517b7bcb01ddc2',
+    pass: '1706173457035c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/config/redis.js
@@ -0,0 +1,4 @@
+export default {
+  host: '192.168.1.100',
+  port: '6379',
+};
